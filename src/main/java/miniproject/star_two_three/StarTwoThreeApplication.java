88325f61c3dad1b719c5b520537519f717b89f62
Chaeyoung714 <202102760@hufs.ac.kr>
2024-12-19 15:09:51 +0900

    initialized

---
+++ src/main/java/miniproject/star_two_three/StarTwoThreeApplication.java
@@ -0,0 +1,13 @@
+package miniproject.star_two_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarTwoThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarTwoThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miniproject/star_two_three/StarTwoThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.star_two_three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarTwoThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
